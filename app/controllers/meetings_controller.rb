225e53409ef11b5cbf5e72c54161ca94ef8a2b0e
Prelang Builder <builder@prelang.com>
2015-03-28 15:07:11 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,4 +1,8 @@
 class MeetingsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_meeting, only: [:show, :edit, :update, :destroy]
 
   # GET /meetings
